8b83e2391ef7872b5c384abc53c9df59ceaf51a9
rl3320 <rl3320@ic.ac.uk>
2023-05-31 18:44:21 +0100

    fix submit button.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    args = request.args.to_dict()
+    if (args.get("job-title") is not None):
+        return redirect("/suggestions")
     return render_template('searchpage.html')
 
 @app.route('/suggestions')
