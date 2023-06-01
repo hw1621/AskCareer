bb59b08ccdd18a06e7dc2a6d0c459fcadcb0bba7
yichenchong <yichenchong@yahoo.com>
2023-06-01 14:14:48 +0100

    Get profile names from database

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request
+import requests
 
 app = Flask(__name__)
 
@@ -8,10 +9,8 @@ def index():
     if request.method == 'POST':
         form = request.form.to_dict()
         print(form)
-        return redirect("/suggestions")
+        be_info = requests.get("https://drp26backend.herokuapp.com/recommend/get_users")
+        print(be_info.content)
+        profiles = [{"name": i["name"]} for i in be_info.json()["profiles"]]
+        return render_template("displaypage.html", profiles=profiles)
     return render_template('searchpage.html')
-
-
-@app.route('/suggestions')
-def suggest():
-    return render_template('displaypage.html')
