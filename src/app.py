3ea7a55972dc134d1dc679519de9af30094b4d24
yichenchong <yichenchong@yahoo.com>
2023-06-06 12:03:14 +0100

    Updated backend-frontend interaction

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import requests
+import json
 
 app = Flask(__name__)
 
@@ -10,13 +11,17 @@ def index():
         form = request.form.to_dict()
         try:
             assert "job-title" in form, "job-title not in form"
-            assert "company" in form, "company not in form"
-            assert "jd" in form, "job-description not in form"
+            assert "job-company" in form, "company not in form"
+            assert "job-summary" in form, "job-description not in form"
         except AssertionError as e:
             return str(e), 500
         print(form)
-        be_info = requests.get("https://drp26backend.herokuapp.com/recommend/get_users")
+        be_info = requests.get(
+            "https://drp26backend.herokuapp.com/recommend/get_users",
+            params=form
+        )
         print(be_info.content)
-        profiles = be_info.json()['profiles']
+        profiles = json.loads(be_info.text)['profiles']
+        print(profiles)
         return render_template("displaypage.html", profiles=profiles)
     return render_template('searchpage.html')
