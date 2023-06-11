0ba78aedd94b01d0f62ddce62217b13812d57390
Mo Zhou <mz1921@ic.ac.uk>
2023-06-11 22:02:04 +0100

    moved the upload_to_s3 function to s3.py

--- src/s3.py
+++ src/s3.py
@@ -1,18 +1,37 @@
 import boto3
-import pandas
+from boto3 import s3
+
 client = boto3.client(
     's3',
-    aws_access_key_id = 'AKIAQXS56FU5AWVPF6UW',
-    aws_secret_access_key = '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',
-    region_name = 'eu-west-2'
+    aws_access_key_id='AKIAQXS56FU5AWVPF6UW',
+    aws_secret_access_key='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',
+    region_name='eu-west-2'
 )
 
 resource = boto3.resource(
     's3',
-    aws_access_key_id = 'AKIAQXS56FU5AWVPF6UW',
-    aws_secret_access_key = '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',
-    region_name = 'eu-west-2'
+    aws_access_key_id='AKIAQXS56FU5AWVPF6UW',
+    aws_secret_access_key='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',
+    region_name='eu-west-2'
 )
+
+
+def save_to_s3(file, bucket_name, acl="public-read"):
+    try:
+        client.upload_fileobj(
+            file,
+            bucket_name,
+            file.filename,
+            ExtraArgs={
+                "ACL": acl,
+                "ContentType": file.content_type
+            }
+        )
+    except Exception as e:
+        print("Exception thrown: ", e)
+        return e
+    return "save successful"
+
 
 if __name__ == "__main__":
     clientResponse = client.list_buckets()
