ecb4aa1d83c92dbfdb49ee2834896a753eb4e289
rl3320 <rl3320@ic.ac.uk>
2023-06-11 12:22:38 +0100

    If do not have work experience, remove the whole work experience element.TODO: add it back if re-choose to have work experince.

--- src/static/js/editprofile.js
+++ src/static/js/editprofile.js
@@ -79,37 +79,15 @@ function deleteWorkField() {
     const length = document.getElementById("work-experience").childNodes.length;
     const lastEduField = document.getElementById("work-experience").childNodes[length-1];
     if (length > 2) {
-        console.log(length);
         lastEduField.remove();
     }
 }
 
+const node = document.getElementById("work-experience");
+
 function hideWorkEntry() {
     if (document.getElementById("not-experienced").checked) {
-        document.getElementById("work-experience").style.display = "none";
-        document.getElementById("work-buttons").style.display = "none";
-        const names = document.getElementsByName("company");
-        for (let i = 0; i < names.length; i++) {
-            names[i].required = false;
-        }
-        const titles = document.getElementsByName("title");
-        for (let i = 0; i < titles.length; i++) {
-            titles[i].required = false;
-        }
-        const summaries = document.getElementsByName("summary");
-        for (let i = 0; i < summaries.length; i++) {
-            summaries[i].required = false;
-        }
-        const startDates = document.getElementsByName("start-date");
-        for (let i = 0; i < startDates.length; i++) {
-            startDates[i].required = false;
-        }
-        const endDates = document.getElementsByName("end-date");
-        for (let i = 0; i < endDates.length; i++) {
-            endDates[i].required = false;
-        }
-    } else {
-        document.getElementById("work-experience").style.display = "block";
-        document.getElementById("work-buttons").style.display = "block";
+        document.getElementById("work-experience").remove();
+        console.log(node);
     }
 }
