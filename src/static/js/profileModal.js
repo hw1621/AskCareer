8b9915f1849ff7ef5d1e9010bb44555378cd3992
Mo Zhou <mz1921@ic.ac.uk>
2023-06-15 14:51:05 +0100

    Added missing URL. before createObjectURL()

--- src/static/js/profileModal.js
+++ src/static/js/profileModal.js
@@ -122,7 +122,7 @@ async function getProfilePhoto(profileId) {
 
 function loadImage(event) {
     image = document.getElementById("edit-profile-image");
-    image.src = createObjectURL(event.target.files[0]);
+    image.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function() {
       URL.revokeObjectURL(output.src) // free memory
     }
